1e43289f3429bf5051a6b5ef074c5ba3d74dd71c
Marenthe de Jong <57255299+marenthedejong@users.noreply.github.com>
2021-09-23 18:27:14 +0000

    Balletje getekend (errors weggewerkt), toch maar vogel.js weggegooid en de code bij script.js gezet.

--- Vogel.js
+++
@@ -1,9 +0,0 @@
-function Vogel() {
-  this.y = 250;
-  this.x = 45; 
-
-  this.create = function() {
-    fill(254, 226, 62)
-    ellipse(this.x, this.y, 20, 20)
-  }
-}
--- script.js
+++ script.js
@@ -1,7 +1,31 @@
-function setup() {
-  createCanvas(500, 600);
+class Vogel{
+
+  constructor(x, y, width, height, c){
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.c = c;
+  }
+
+  draw(){
+    fill(this.c);
+    ellipse(this.x, this.y, this.width, this.height, this.c);
+  
+  }
+  
 }
 
-function draw() {
-  background(205, 240, 255);
+var vogel1;
+
+function setup() {
+  createCanvas(500, 600);
+  vogel1 = new Vogel(250, 45, 20, 20, "yellow");
 }
+
+function draw(){
+  background(205, 240, 255);
+  vogel1.draw();
+ }
+
+
