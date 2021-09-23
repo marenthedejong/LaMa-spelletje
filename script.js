a3a42e934c07b928a81ab745567fc38e3f9314c5
Marenthe de Jong <57255299+marenthedejong@users.noreply.github.com>
2021-09-23 18:44:04 +0000

    Zwaartekracht en snelheid toegevoegd aan de vogel.

--- script.js
+++ script.js
@@ -1,31 +1,55 @@
 class Vogel{
 
-  constructor(x, y, width, height, c){
+  constructor(x, y, width, height, c, gravity, v){
     this.x = x;
     this.y = y;
     this.width = width;
     this.height = height;
     this.c = c;
+    this.gravity = gravity;
+    this.v = v;
   }
 
   draw(){
     fill(this.c);
-    ellipse(this.x, this.y, this.width, this.height, this.c);
+    ellipse(this.x, this.y, this.width, this.height, this.c, this.gravity, this.v);
+    
   
   }
-  
+
+  update() {
+    this.v += this.gravity;
+    this.y += this.v;
+
+
+    if (this.y >= height){
+      this.y = height;
+
+      this.v = 0;
+
+    }
+
+    if (this.y >= 0){
+      this.y = height;
+
+      this.v = 0;
+      
+    }
+  }
 }
 
-var vogel1;
+var vogel;
 
 function setup() {
   createCanvas(500, 600);
-  vogel1 = new Vogel(250, 45, 20, 20, "yellow");
+  vogel = new Vogel(250, 45, 20, 20, "yellow", 0.1, 0);
 }
 
 function draw(){
   background(205, 240, 255);
-  vogel1.draw();
- }
+  vogel.draw();
+  vogel.update();
+}
+
 
 
